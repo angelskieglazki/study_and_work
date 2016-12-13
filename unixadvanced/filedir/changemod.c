bf1e10963f9d08fe4bd7b7479c33eaf28420ba58
Max <max.ponka@hotmail.com>
2016-12-13 23:44:51 +0300

    some question

---
+++ unixadvanced/filedir/changemod.c
@@ -0,0 +1,15 @@
+#include "apue.h"
+
+int main(void) {
+    struct stat statbuf;
+
+    if (stat("foo", &statbuf) < 0)
+        err_sys("Error to call stat for foo file");
+    if (chmod("foo", (statbuf.st_mode & S_IXGRP) | S_ISGID) < 0)
+        err_sys("Error to call chmod for foo file");
+
+    if (chmod("bar", S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH) < 0)
+        err_sys("Error to call chmod for bar file");
+
+    exit(0);        
+}
