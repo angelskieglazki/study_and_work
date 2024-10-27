8eefd73cf8b255ac58bbf7eff84ed0a9097ed1df
max <mpano91@gmail.com>
2024-10-27 17:16:35 +0300

    update

---
+++ sandbox/opengl/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install build-essential libgl1-mesa-dev libglew-dev libsdl2-dev libsdl2-image-dev libglm-dev libfreetype6-dev build-essential libgl1-mesa-dev libglew-dev libglfw3-dev libglm-dev
